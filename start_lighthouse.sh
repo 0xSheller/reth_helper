438bc6351de9b04742b1fb3748e5d7115b80d838
0xSheller <93097065+0xSheller@users.noreply.github.com>
2023-06-25 21:38:04 -0700

    fix timeouts

--- start_lighthouse.sh
+++ start_lighthouse.sh
@@ -3,4 +3,4 @@
 # Load the environment variables
 source load_variables.sh
 
-/$BASE_DIR/lighthouse/lighthouse beacon_node --datadir /$BASE_DIR/lighthouse/data --network mainnet --http --execution-endpoint http://localhost:8551 --execution-jwt /$BASE_DIR/$NODE_CLIENT/data/jwt.hex --checkpoint-sync-url https://mainnet-checkpoint-sync.stakely.io --disable-backfill-rate-limiting --genesis-backfill --disable-deposit-contract-sync --execution-timeout-multiplier 10
+sudo /$BASE_DIR/lighthouse/lighthouse beacon_node --datadir /$BASE_DIR/lighthouse/data --network mainnet --http --execution-endpoint http://localhost:8551 --execution-jwt /$BASE_DIR/$NODE_CLIENT/data/jwt.hex --checkpoint-sync-url https://mainnet-checkpoint-sync.stakely.io --disable-backfill-rate-limiting --genesis-backfill --disable-deposit-contract-sync --execution-timeout-multiplier 10
