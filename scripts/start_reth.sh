ff267176f775233c7ebea80f91b2d6ef901f7847
0xSheller <93097065+0xSheller@users.noreply.github.com>
2023-06-29 16:54:53 -0700

    Update start_reth.sh

--- scripts/start_reth.sh
+++ scripts/start_reth.sh
@@ -6,4 +6,4 @@ parsed_dir="/${dir_array[0]}/${dir_array[1]}/${dir_array[2]}"
 source "${parsed_dir}/scripts/load_variables.sh"
 source "${parsed_dir}/scripts/get_os_arch.sh"
 
-sudo RUST_LOG=info /$BASE_DIR/$NODE_CLIENT/$NODE_CLIENT node --datadir=/$BASE_DIR/$NODE_CLIENT/data --chain=mainnet --metrics=5005 --port=30303 --http --ws --log.persistent --log.directory=/$BASE_DIR/$NODE_CLIENT/logs/
+sudo RUST_LOG=info /$BASE_DIR/$NODE_CLIENT/$NODE_CLIENT node --datadir=/$BASE_DIR/$NODE_CLIENT/data --chain=mainnet --metrics=5005 --port=30303 --http --http.api "debug,eth,net,trace,txpool,web3,rpc" --ws --log.persistent --log.directory=/$BASE_DIR/$NODE_CLIENT/logs/
